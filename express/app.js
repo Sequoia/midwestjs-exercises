c6cbac7420673526a2f3a9e91609f588685b5087
Sequoia McDowell <sequoia.mcdowell@gmail.com>
2015-08-06 17:52:28 -0400

    post route

--- express/app.js
+++ express/app.js
@@ -1,11 +1,22 @@
 // in app.js
 var express = require('express');
 var myApp = express();
+var bodyParser = require('body-parser');
+
+myApp.use(bodyParser.urlencoded({extended: false}));
+myApp.use(bodyParser.json());
 
 myApp.use(express.static('../httpserver/public'));
 
 myApp.get('/', function handleRoot(req, res, next) {
     res.send('Hello World!');
+});
+
+myApp.post('/widgets', function handleRoot(req, res, next) {
+  console.log('posted some data!');
+  console.log(req.body);
+  //echo request data to client
+  res.json(req.body);
 });
 
 myApp.listen(8080);
