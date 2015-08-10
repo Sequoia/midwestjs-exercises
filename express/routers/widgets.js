72abb21809e15d0eabb419305d611a0246b36534
Sequoia McDowell <sequoia.mcdowell@gmail.com>
2015-08-10 15:11:16 -0400

    create, get, list widgets

--- express/routers/widgets.js
+++ express/routers/widgets.js
@@ -1,15 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var _ = require('lodash');
 
-router.post('/',function handleRoot(req, res, next) {
-  console.log('posted some data!');
-  console.log(req.body);
-  //echo request data to client
-  res.json(req.body);
+var widgets = [];
+var lastId  = 0;
+
+router.get('/',function getAll(req, res, next) {
+  console.log('yo');
+  if(widgets.length){
+    res.render('all_widgets', {widgets: widgets});
+  }else{
+    res.redirect('create');
+  }
 });
 
-router.get('/create',function handleRoot(req, res, next) {
+router.post('/',function handleRoot(req, res, next) {
+  var widget = {
+    name : req.body.name,
+    id   : ++lastId
+  };
+  widgets.push(widget);
+  res.render('all_widgets', {widgets: widgets});
+});
+
+router.get('/create',function getForm(req, res, next) {
   res.render('widget_form');
+});
+
+router.get('/:id(\\d+)/', function getOne(req, res, next){
+  var widget = _.find(widgets, {id : parseInt(req.params.id)});
+  if(widget){
+    res.render('widget',widget);
+  }else{
+    var e = new Error('Could not find widget');
+    e.code = 404;
+    next(e);
+  }
 });
 
 module.exports = router;
